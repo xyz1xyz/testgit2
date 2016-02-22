53e27694781cde5bc8f12d512034f38c6fcfd10e
xyz1xyz <1457988631@qq.com>
2016-02-22 14:58:43 +0800

    kggg第一次提交

---
+++ kggg/src/main/java/kggg/kggg/App.java
@@ -0,0 +1,13 @@
+package kggg.kggg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
